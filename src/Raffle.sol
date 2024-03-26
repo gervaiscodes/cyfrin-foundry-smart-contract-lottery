4bbb95e04d50b3ee56767bbc499d7b81d9a1c1a9
Gervais Codes <gervaiscodes@protonmail.com>
2024-03-26 17:58:51 +0100

    Enter raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,14 +3,24 @@
 pragma solidity ^0.8.19;
 
 contract Raffle {
+    error Raffle__NotEnoughEthSent();
+
     uint256 private i_entranceFee;
+    address payable[] private s_players;
+
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
+        if(msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
 
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {
