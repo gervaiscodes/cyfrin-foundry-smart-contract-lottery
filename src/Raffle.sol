0cd540cee71a336e2b1239f13154eb022c098937
Gervais Codes <gervaiscodes@protonmail.com>
2024-03-26 17:41:01 +0100

    Set entrance fee

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,5 +3,21 @@
 pragma solidity ^0.8.19;
 
 contract Raffle {
+    uint256 private i_entranceFee;
 
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+
+    }
+
+    function pickWinner() public {
+
+    }
+
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
 }
