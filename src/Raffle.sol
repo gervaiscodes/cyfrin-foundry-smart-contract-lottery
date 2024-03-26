0a23b18b77b33f3a35832c5abada7e3c17d8051a
Gervais Codes <gervaiscodes@protonmail.com>
2024-03-26 18:04:41 +0100

    Pick winner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -5,13 +5,17 @@ pragma solidity ^0.8.19;
 contract Raffle {
     error Raffle__NotEnoughEthSent();
 
-    uint256 private i_entranceFee;
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimestamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimestamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -23,8 +27,10 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {
-
+    function pickWinner() external {
+        if(block.timestamp - s_lastTimestamp < i_interval) {
+            revert();
+        }
     }
 
     function getEntranceFee() external view returns(uint256) {
